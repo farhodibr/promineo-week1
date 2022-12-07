b6853fbea034ad2cff44895487dcd8db9039e085
farhodibr <farhodibr@gmail.com>
2022-12-06 21:03:55 -0500

    save

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 console.log("It works! What's up. How are you doing FArhod?")
 
+// This is my comment
 
+ var myName="Farhod"
+let myName2="Frank"
+const lastName="Ibragimov"
+
+console.log (myName,myName2,lastName)
+
+ myName="Farik"
+
+ console.log(myName)
+ var age ="40"
+ var isCool=true
+ var otherAge=50
+ console.log(age+otherAge+myName+" "+myName2+" "+lastName)
